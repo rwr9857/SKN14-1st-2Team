b65a3af7d1f1fc4d93193f5857277b42e5587283
my-cookies-26 <my_cookies_@naver.com>
2025-04-28 15:31:11 +0900

    Feat: db 수정

--- DB/init.sql
+++ DB/init.sql
@@ -5,8 +5,8 @@
 # DROP TABLE IF EXISTS BODY_TYPE_INFO;
 # DROP TABLE IF EXISTS ENGINE_INFO;
 # DROP TABLE IF EXISTS BRAND_INFO;
-# drop table if exists car_review_info;
-# drop table if exists comment_info;
+drop table if exists car_review_info;
+drop table if exists comment_info;
 # drop table if exists job_type_info;
 
 -- 직업정보
@@ -31,7 +31,7 @@ CREATE TABLE car_recommendation_info (
     user_id INT NOT NULL,
     car_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user_info(user_id),
-    FOREIGN KEY (car_id) REFERENCES all_data(id)
+    FOREIGN KEY (car_id) REFERENCES car_info(car_id)
 );
 
 -- 차의 리뷰
@@ -42,7 +42,7 @@ create table car_review_info (
     avg_score float not null,
     survey_people_count int,
     graph_info varchar(100),
-    foreign key (car_id) references all_data(id)
+    foreign key (car_id) references car_info(car_id)
 );
 
 -- 리뷰의 댓글
@@ -63,3 +63,6 @@ INSERT INTO JOB_TYPE_INFO (job_name) VALUES
 ('생산직'),
 ('기타');
 
+SHOW TABLES;
+SHOW CREATE TABLE 테이블명;
+DESCRIBE teamdb;
