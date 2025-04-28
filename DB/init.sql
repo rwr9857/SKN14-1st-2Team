b04c0e9456bb596aa4904117005732dc4cff077f
my-cookies-26 <my_cookies_@naver.com>
2025-04-28 11:38:13 +0900

    Feat: init 수정

--- DB/init.sql
+++ DB/init.sql
@@ -9,55 +9,10 @@
 # drop table if exists comment_info;
 # drop table if exists job_type_info;
 
--- 브랜드 정보
-CREATE TABLE BRAND_INFO (
-    BRAND_ID INT AUTO_INCREMENT PRIMARY KEY ,
-    BRAND_NAME VARCHAR(30) NOT NULL
-);
-
--- 바디 타입 정보
-CREATE TABLE BODY_TYPE_INFO (
-    BODY_TYPE_ID INT AUTO_INCREMENT PRIMARY KEY ,
-    BODY_TYPE_NAME VARCHAR(30) NOT NULL
-);
-
--- 모델명 정보
-CREATE TABLE MODEL_TYPE_INFO (
-    MODEL_TYPE_ID INT AUTO_INCREMENT PRIMARY KEY ,
-    MODEL_TYPE_NAME VARCHAR(30) NOT NULL
-);
-
--- 연료 정보
-CREATE TABLE FUEL_TYPE_INFO (
-    FUEL_TYPE_ID INT AUTO_INCREMENT PRIMARY KEY ,
-    FUEL_TYPE_NAME VARCHAR(30) NOT NULL
-);
-
 -- 직업정보
 create table JOB_TYPE_INFO (
     job_id int auto_increment primary key ,
     job_name varchar(20)
-);
-
--- 자동차 정보
-CREATE TABLE CAR_INFO (
-    CAR_ID INT AUTO_INCREMENT PRIMARY KEY ,
-    CAR_FULL_NAME VARCHAR(100) ,
-    CAR_MODEL INT,
-    CAR_BRAND INT,
-    CAR_BODY_TYPE INT,
-    CAR_FUEL_TYPE INT,
-    CAR_PRICE INT,
-    CAR_HORSEPOWER FLOAT,
-    CAR_FUEL_EFFICIENCY FLOAT,
-    CAR_MODEL_YEAR INT,
-    CAR_SIZE FLOAT,
-    CAR_ENGINE_TYPE varchar(40),
-    CAR_IMG_URL VARCHAR(100),
-    FOREIGN KEY (CAR_MODEL) REFERENCES MODEL_TYPE_INFO(MODEL_TYPE_ID),
-    FOREIGN KEY (CAR_BRAND) REFERENCES  BRAND_INFO(BRAND_ID),
-    FOREIGN KEY (CAR_BODY_TYPE) REFERENCES BODY_TYPE_INFO(BODY_TYPE_ID),
-    FOREIGN KEY (CAR_FUEL_TYPE) REFERENCES FUEL_TYPE_INFO(FUEL_TYPE_ID)
 );
 
 -- 유저 정보
@@ -76,7 +31,7 @@ CREATE TABLE car_recommendation_info (
     user_id INT NOT NULL,
     car_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user_info(user_id),
-    FOREIGN KEY (car_id) REFERENCES car_info(car_id)
+    FOREIGN KEY (car_id) REFERENCES all_data(id)
 );
 
 -- 차의 리뷰
@@ -87,7 +42,7 @@ create table car_review_info (
     avg_score float not null,
     survey_people_count int,
     graph_info varchar(100),
-    foreign key (car_id) references car_info(car_id)
+    foreign key (car_id) references all_data(id)
 );
 
 -- 리뷰의 댓글
